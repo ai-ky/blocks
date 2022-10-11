42c365946bbeb72d1bb2e5ea12f846301bb0201e
ai-ky <113825993+ai-ky@users.noreply.github.com>
2022-10-11 14:18:17 +0800

    Add files via upload

--- py/code/block_python_functions.js
+++ py/code/block_python_functions.js
@@ -69,3 +69,43 @@ Blockly.Python['python_ord'] = function(block) {
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
+//========================================================
+Blockly.Blocks['python_time_import'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("import time");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(python_functions_colour);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.Python['python_time_import'] = function(block) {
+  // TODO: Assemble Python into code variable.
+  var code = 'import time\n';
+  return code;
+};
+//========================================================
+Blockly.Blocks['python_time_sleep'] = {
+  init: function() {
+    this.appendValueInput("NAME")
+        .setCheck(null)
+        .appendField("time.sleep(");
+    this.appendDummyInput()
+        .appendField(")");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(python_functions_colour);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.Python['python_time_sleep'] = function(block) {
+  var value_name = Blockly.Python.valueToCode(block, 'NAME', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = 'time.sleep('+value_name+')\n';
+  return code;
+};
