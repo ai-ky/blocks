95cd1d05084af22d6cb34c90795e51b4876e1493
ai-ky <113825993+ai-ky@users.noreply.github.com>
2023-06-01 00:14:12 +0800

    Add files via upload

--- develop tools/compressXML.py
+++ develop tools/compressXML.py
@@ -1,6 +1,10 @@
+#將剪貼簿中之 block html 化簡並縮為一行
 import pyperclip
 text=pyperclip.paste()
 S=''
+i=text.find("<block")
+j=text.find("</xml>")
+text = text[i:j]
 for line in text.split("\n"):
     ii=line.find('id=')
     
@@ -8,5 +12,6 @@ for line in text.split("\n"):
         jj=line.find('>',ii)
         S+=line[:ii].strip()+line[jj:].strip()
     else: S+=line.strip()
+
 print(S)
 pyperclip.copy(S)
