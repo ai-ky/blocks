1c9b62b38f528afca7fe63c1a68bec3b28749eb3
K.Y. Winston Lee <113825993+ai-ky@users.noreply.github.com>
2023-10-25 06:39:15 +0800

    add esp32通訊

--- arduino/code/block_arduino_c.js
+++ arduino/code/block_arduino_c.js
@@ -146,7 +146,7 @@ Blockly.Python['block_parentheses'] = function(block) {
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
 //======================================
-Blockly.Blocks['python_big_parantheses'] = {
+Blockly.Blocks['block_big_parantheses'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("{");
@@ -161,7 +161,7 @@ Blockly.Blocks['python_big_parantheses'] = {
  this.setHelpUrl("");
   }
 };
-Blockly.Python['python_big_parantheses'] = function(block) {
+Blockly.Python['block_big_parantheses'] = function(block) {
   var value_name1 = Blockly.Python.valueToCode(block, 'NAME1', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
   var code = '{'+value_name1+'}';
@@ -387,7 +387,7 @@ Blockly.Python['block_serial_'] = function(block) {
   var dropdown_name = block.getFieldValue('NAME2');
   var value_name = Blockly.Python.valueToCode(block, 'NAME1', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = 'Serial.'+dropdown_name+'('+value_name+')';;
+  var code = 'Serial.'+dropdown_name+'('+value_name+')';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
@@ -427,10 +427,11 @@ Blockly.Blocks['block_digitalread'] = {
         .setCheck(null)
         .appendField("digitalRead(");
     this.appendDummyInput()
-        .appendField(");");
+        .appendField(")");
     this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
+	this.setOutput(true, null);
+    //this.setPreviousStatement(true, null);
+    //this.setNextStatement(true, null);
     this.setColour(80);
  this.setTooltip("");
  this.setHelpUrl("");
@@ -439,8 +440,8 @@ Blockly.Blocks['block_digitalread'] = {
 Blockly.Python['block_digitalread'] = function(block) {
   var value_name1 = Blockly.Python.valueToCode(block, 'NAME1', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = "digitalRead("+value_name1+');\n';
-  return code;
+  var code = "digitalRead("+value_name1+')';
+  return [code, Blockly.Python.ORDER_ATOMIC];
 };
 //<block type="block_highlow"></block>
 //======================================
@@ -499,10 +500,11 @@ Blockly.Blocks['block_analogread'] = {
         .setCheck(null)
         .appendField("analogRead(");
     this.appendDummyInput()
-        .appendField(");");
+        .appendField(")");
     this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
+	this.setOutput(true, null);
+    //this.setPreviousStatement(true, null);
+    //this.setNextStatement(true, null);
     this.setColour(80);
  this.setTooltip("");
  this.setHelpUrl("");
@@ -511,8 +513,8 @@ Blockly.Blocks['block_analogread'] = {
 Blockly.Python['block_analogread'] = function(block) {
   var value_name1 = Blockly.Python.valueToCode(block, 'NAME1', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = "analogRead("+value_name1+');\n';
-  return code;
+  var code = "analogRead("+value_name1+')';
+  return [code, Blockly.Python.ORDER_ATOMIC];
 };
 
 //<block type="block_semicolon"></block>
@@ -823,6 +825,117 @@ Blockly.Python['block_comment_'] = function(block) {
   var text_name = block.getFieldValue('NAME');
   // TODO: Assemble Python into code variable.
   var code = text_name;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+//<block type="block_func1"></block>
+//======================================
+Blockly.Blocks['block_func1'] = {
+  init: function() {
+    this.appendValueInput("NAME1")
+        .setCheck(null);
+    this.appendValueInput("NAME2")
+        .setCheck(null)
+        .appendField("(");
+    this.appendDummyInput()
+        .appendField(")");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(80);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.Python['block_func1'] = function(block) {
+  var value_name1 = Blockly.Python.valueToCode(block, 'NAME1', Blockly.Python.ORDER_ATOMIC);
+  var value_name2 = Blockly.Python.valueToCode(block, 'NAME2', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = value_name1+'('+value_name2+')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+//<block type="block_func2"></block>
+//======================================
+Blockly.Blocks['block_func2'] = {
+  init: function() {
+    this.appendValueInput("NAME1")
+        .setCheck(null);
+    this.appendValueInput("NAME2")
+        .setCheck(null)
+        .appendField("(");
+    this.appendDummyInput()
+        .appendField(",");
+    this.appendValueInput("NAME3")
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField(")");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(80);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.Python['block_func2'] = function(block) {
+  var value_name1 = Blockly.Python.valueToCode(block, 'NAME1', Blockly.Python.ORDER_ATOMIC);
+  var value_name2 = Blockly.Python.valueToCode(block, 'NAME2', Blockly.Python.ORDER_ATOMIC);
+  var value_name3 = Blockly.Python.valueToCode(block, 'NAME3', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code =  value_name1+'('+value_name2+','+value_name3+')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+//<block type="block_big_parantheses_statement"></block>
+//======================================
+Blockly.Blocks['block_big_parantheses_statement'] = {
+  init: function() {
+    this.appendValueInput("NAME1")
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField("{");
+    this.appendStatementInput("NAME2")
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField("}");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(80);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.Python['block_big_parantheses_statement'] = function(block) {
+  var value_name1 = Blockly.Python.valueToCode(block, 'NAME1', Blockly.Python.ORDER_ATOMIC);
+  var statements_name2 = Blockly.Python.statementToCode(block, 'NAME2');
+  // TODO: Assemble Python into code variable.
+  var code = value_name1+'{\n'+statements_name2+'}\n';
+  return code;
+};
+
+//<block type="block_dot1"></block>
+//======================================
+Blockly.Blocks['block_dot1'] = {
+  init: function() {
+    this.appendValueInput("NAME1")
+        .setCheck(null);
+    this.appendValueInput("NAME2")
+        .setCheck(null)
+        .appendField(".");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(80);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.Python['block_dot1'] = function(block) {
+  var value_name1 = Blockly.Python.valueToCode(block, 'NAME1', Blockly.Python.ORDER_ATOMIC);
+  var value_name2 = Blockly.Python.valueToCode(block, 'NAME2', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = value_name1+'.'+value_name2;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
